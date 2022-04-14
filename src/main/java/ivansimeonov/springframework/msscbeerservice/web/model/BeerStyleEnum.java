dd8e53f8db82163319534ccda698577fff34a747
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-14 12:08:42 +0300

    Beer Model added

---
+++ src/main/java/ivansimeonov/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,20 @@
+package ivansimeonov.springframework.msscbeerservice.web.model;
+
+/**
+ * @Author ivansimeonov
+ * @Date 14.04.22
+ */
+public enum BeerStyleEnum {
+    LAGER,
+    PALE_ALE,
+    ALE,
+    WITBIER,
+    STOUT,
+    PILSNER,
+    PORTER,
+    BITTER,
+    GOSE,
+    WHEAT,
+    IPA,
+    SAISON
+}
