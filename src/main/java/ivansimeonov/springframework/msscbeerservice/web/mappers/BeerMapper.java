01f7f1eb32c896a19aca6ae533805f800fc728d3
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-22 18:30:34 +0300

    MapStruct fixed

--- src/main/java/ivansimeonov/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/ivansimeonov/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -3,7 +3,6 @@ package ivansimeonov.springframework.msscbeerservice.web.mappers;
 import ivansimeonov.springframework.msscbeerservice.domain.Beer;
 import ivansimeonov.springframework.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 /**
  * @Author ivansimeonov
@@ -11,12 +10,8 @@ import org.mapstruct.Mapping;
  */
 @Mapper(uses = DateMapper.class, componentModel = "spring")
 public interface BeerMapper {
-    @Mapping(target = "quantityOnHand", source = "")
-    @Mapping(target = "lastModifiedDate", source = "")
+
     BeerDto beerToBeerDto(Beer beer);
 
-    @Mapping(target = "quantityToBrew", source = "")
-    @Mapping(target = "modifiedDate", source = "")
-    @Mapping(target = "minOnHand", source = "")
     Beer beerDtoToBeer(BeerDto beerDto);
 }
