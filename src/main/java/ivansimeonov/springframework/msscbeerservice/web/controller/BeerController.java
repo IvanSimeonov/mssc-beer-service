326f6bbb5e6d8a692ac4c07ed9015b5cb2161720
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-24 21:08:02 +0300

    Page controller methods added

--- src/main/java/ivansimeonov/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/ivansimeonov/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,8 +1,12 @@
 package ivansimeonov.springframework.msscbeerservice.web.controller;
 
+import ivansimeonov.springframework.msscbeerservice.domain.Beer;
 import ivansimeonov.springframework.msscbeerservice.services.BeerService;
 import ivansimeonov.springframework.msscbeerservice.web.model.BeerDto;
+import ivansimeonov.springframework.msscbeerservice.web.model.BeerPagedList;
+import ivansimeonov.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,11 +24,35 @@ import java.util.UUID;
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 20;
+
     private final BeerService beerService;
+
+    @GetMapping(produces = {"application/json"}, path = "beer")
+    public ResponseEntity<BeerPagedList> beersList(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                                   @RequestParam(value = "pageSize", required = false) Integer pageSize,
+                                                   @RequestParam(value = "beerName", required = false) String beerName,
+                                                   @RequestParam(value = "beerStyle", required = false) BeerStyleEnum beerStyle) {
+        if (pageNumber == null || pageNumber < 0) {
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+        if (pageSize == null || pageSize < 0) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
+
+        BeerPagedList beersList = beerService.beersList(beerName, beerStyle, PageRequest.of(pageNumber, pageSize));
+        return new ResponseEntity<>(beersList, HttpStatus.OK);
+    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getById(beerId), HttpStatus.OK);
+    }
+
+    @GetMapping("beerUpc/{upc}")
+    public ResponseEntity<BeerDto> getBeerByUpc(@PathVariable("upc") String upc) {
+        return new ResponseEntity<>(beerService.getByUpc(upc), HttpStatus.OK);
     }
 
     @PostMapping
