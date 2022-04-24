3a0ffc994ac52349e044cad067d64e11840f2d3d
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-24 20:55:41 +0300

    Page methods added to the service; Component annotation added to the mapper interface

--- src/main/java/ivansimeonov/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/ivansimeonov/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -3,11 +3,13 @@ package ivansimeonov.springframework.msscbeerservice.web.mappers;
 import ivansimeonov.springframework.msscbeerservice.domain.Beer;
 import ivansimeonov.springframework.msscbeerservice.web.model.BeerDto;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author ivansimeonov
  * @Date 20.04.22
  */
+@Component
 @Mapper(uses = DateMapper.class, componentModel = "spring")
 public interface BeerMapper {
 
